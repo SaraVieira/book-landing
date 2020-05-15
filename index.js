0c3e6c3c5a1ef5bf4632ebc785994e8c751bd4b9
Sara Vieira <hey@iamsaravieira.com>
2020-05-16 00:10:49 +0200

    start

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Yeah this site is not written in React");
+console.log("Actually thats the only JS I wrote for it.");
+
+console.log(
+  "If you found this I guess I need to share a code right? When it's out I will make the code `iseeeverything` code and you will get 20% off"
+);
