1b7c7242d3266a4902e619d7d6cd32e25bfd6d56
Sara Vieira <hey@iamsaravieira.com>
2020-05-16 00:44:32 +0200

    add thabnk you

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+import ConfettiGenerator from "confetti-js";
+
 console.log("Yeah this site is not written in React");
-console.log("Actually thats the only JS I wrote for it.");
 
 console.log(
   "If you found this I guess I need to share a code right? When it's out I will make the code `iseeeverything` code and you will get 20% off"
 );
+
+var confettiSettings = {
+  target: "confetti-holder",
+  max: "800",
+  size: "1",
+  animate: true,
+  props: ["circle", "square", "triangle", "line"],
+  colors: [
+    [165, 104, 246],
+    [230, 61, 135],
+    [0, 199, 228],
+    [253, 214, 126],
+  ],
+  clock: "25",
+  rotate: false,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  start_from_edge: false,
+  respawn: true,
+};
+var confetti = new ConfettiGenerator(confettiSettings);
+confetti.render();
